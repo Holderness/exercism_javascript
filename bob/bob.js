10465b4991d017a61b21445462cd6bf88ce165f1
Holderness <andersonholderness@gmail.com>
2015-02-15 21:33:45 -0500

    adds bob refactor2

--- bob/bob.js
+++ bob/bob.js
@@ -1,24 +1,15 @@
-//
-// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 
 var Bob = function() {};
 
 Bob.prototype.hey = function(phrase) {
-  if (isYelling(phrase)) {
-    return 'Whoa, chill out!';
-  } else if (isQuestion(phrase)) {
-    return "Sure.";
-  } else if (isSilence(phrase)) {
-    return "Fine. Be that way!";
-  } else {
-    return 'Whatever.';
-  }
+  if (isYelling(phrase)) return 'Whoa, chill out!';
+  if (isQuestion(phrase)) return "Sure.";
+  if (isSilence(phrase)) return "Fine. Be that way!";
+  return 'Whatever.';
 };
 
 function isYelling(phrase) {
-	return (phrase === phrase.toUpperCase()) && (phrase != phrase.toLowerCase());
+	return (phrase === phrase.toUpperCase()) && (phrase !== phrase.toLowerCase());
 }
 
 function isQuestion(phrase) {
@@ -28,6 +19,5 @@ function isQuestion(phrase) {
 function isSilence(phrase) {
 	return !phrase.trim();
 }
-
 
 module.exports = Bob;
