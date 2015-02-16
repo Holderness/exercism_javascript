50a41d77bf56ad051ac735509c019109a21859f3
Holderness <andersonholderness@gmail.com>
2015-02-15 20:34:07 -0500

    adds first JS exercise

---
+++ bob/bob.js
@@ -0,0 +1,21 @@
+//
+// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Bob = function() {};
+
+Bob.prototype.hey = function(input) {
+  if ((input === input.toUpperCase()) && (input != input.toLowerCase())) {
+    return 'Whoa, chill out!';
+  } else if (input.slice(-1).indexOf("?") >= 0) {
+    return "Sure.";
+  } else if (!input.trim()) {
+    return "Fine. Be that way!";
+  } else {
+    return 'Whatever.';
+  }
+};
+
+
+module.exports = Bob;
