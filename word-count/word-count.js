506c2c04adb3ea4d0a24f0ee0eb73924e0717d76
Holderness <andersonholderness@gmail.com>
2015-02-17 19:30:15 -0500

    adds refactor 1 of word-count

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -3,13 +3,9 @@
 var words = function words(str) {
   var wordBank = str.split(/\s+/);
   var wordCount = {};
-  for (i = 0; i < wordBank.length; i++) {
-    if (!isNaN(wordCount[wordBank[i]])) {
-      wordCount[wordBank[i]]++;
-    } else {
-      wordCount[wordBank[i]] = 1;
-    }
-  }
+  wordBank.forEach(function(word){
+    wordCount[word] = (!isNaN(wordCount[word]) ? wordCount[word]+1 : 1);
+  });
   return wordCount;
 };
 
