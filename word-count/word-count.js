ca86f6f6f19a0409076c4f463f23b82b5a6f55af
Holderness <andersonholderness@gmail.com>
2015-02-17 19:15:19 -0500

    fixes indentation

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -1,16 +1,16 @@
 
 
 var words = function words(str) {
-	var wordBank = str.split(/\s+/);
-	var wordCount = {};
-	for (i = 0; i < wordBank.length; i++) {
-    if (!isNaN(wordCount[wordBank[i]])){
+  var wordBank = str.split(/\s+/);
+  var wordCount = {};
+  for (i = 0; i < wordBank.length; i++) {
+    if (!isNaN(wordCount[wordBank[i]])) {
       wordCount[wordBank[i]]++;
     } else {
       wordCount[wordBank[i]] = 1;
     }
-	}
-	return wordCount;
+  }
+  return wordCount;
 };
 
 module.exports = words;
