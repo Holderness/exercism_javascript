6ee7c5c53b36c31061fabc3664ad2d3b9bd08915
Holderness <andersonholderness@gmail.com>
2015-02-17 22:18:41 -0500

    adds hamming.js refactor

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -1,14 +1,17 @@
 var Hamming = {
+
   compute: function(strand1, strand2) {
-    var diff = [];
-    if (strand1.length === strand2.length) {
-      strand1.split("").forEach(function(char, i) {
-        if (char != strand2[i]) diff.push(char);
-      });
-    } else {
+
+    if (strand1.length != strand2.length) {
       throw new Error('DNA strands must be of equal length.');
     }
-    return diff.length;
+
+    var diff = 0;
+    strand1.split("").forEach(function(char, i) {
+      if (char != strand2[i]) diff++;
+    });
+    
+    return diff;
   }
 };
 
