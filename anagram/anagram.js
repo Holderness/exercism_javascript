59603ff44473286ccfdab9b022f4e2248b38f77e
Holderness <andersonholderness@gmail.com>
2015-02-18 22:32:25 -0500

    adds refactor, cleans up code

--- anagram/anagram.js
+++ anagram/anagram.js
@@ -1,29 +1,30 @@
 
-var anagram = function(gram){
-	var anagramz = gram;
-  function findAnagrams(wordList) {
-    if (typeof wordList === "string") {
-      list = [];
-      for (var i=0; i<arguments.length; i++) {
-        list.push(arguments[i]);
-      }
-      wordList = list;
-    }
-    var anagrams = [];
-    wordList.forEach(function(word) {
-      if (gram === word.toLowerCase()) return word;
-      if (normalize(word) === normalize(anagramz)) return anagrams.push(word);
-    });
-    return anagrams;
-  }
+var anagram = function anagram(word){
+	anagram.word = word;
   return {
     matches: findAnagrams
   };
 };
 
+function findAnagrams(wordList) {
+  wordList = Array.isArray(wordList) ? wordList : [].slice.call(arguments);
+  return filterWords(wordList);
+}
+
+function filterWords(wordList){
+	return wordList.filter(function(word){
+    return isNotSameWord(word, anagram.word) && (normalize(word) === normalize(anagram.word));
+  });
+}
+
+function isNotSameWord(word, anagram){
+	return word.toLowerCase() != anagram.toLowerCase();
+}
 
 function normalize(string){
 	return string.toLowerCase().split("").sort().join("");
 }
 
 module.exports = anagram;
+
+
