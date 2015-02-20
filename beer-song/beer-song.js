f431ccf07af67b4f76aa8d94102fcbc53980b060
Holderness <andersonholderness@gmail.com>
2015-02-20 15:43:47 -0500

    refactors formating for readability

--- beer-song/beer-song.js
+++ beer-song/beer-song.js
@@ -1,10 +1,18 @@
 var Beer = {
   verse: function(bottles){
-    if (bottles === 2) return "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n";
-    if (bottles === 1) return "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n";
-    if (bottles === 0) return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n";
     var oneLessBottle = bottles-1;
-    return bottles + " bottles of beer on the wall, " + bottles + " bottles of beer.\nTake one down and pass it around, "+ oneLessBottle + " bottles of beer on the wall.\n";
+    if (bottles === 2) return "2 bottles of beer on the wall, 2 " +
+      "bottles of beer.\nTake one down and pass it around, 1 " +
+      "bottle of beer on the wall.\n";
+    if (bottles === 1) return "1 bottle of beer on the wall, 1 " +
+      "bottle of beer.\nTake it down and pass it around, no more " +
+      "bottles of beer on the wall.\n";
+    if (bottles === 0) return "No more bottles of beer on the wall, " +
+      "no more bottles of beer.\nGo to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n";
+    return bottles + " bottles of beer on the wall, " + bottles +
+      " bottles of beer.\nTake one down and pass it around, " +
+      oneLessBottle + " bottles of beer on the wall.\n";
   },
   sing: function(bottleStart, bottleEnd){
     if (isNaN(bottleEnd)) bottleEnd = 0;
