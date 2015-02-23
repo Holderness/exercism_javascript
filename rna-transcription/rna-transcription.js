034c4082c6b5ebbf9595815c8cfa71a870509d7e
Holderness <andersonholderness@gmail.com>
2015-02-23 07:58:19 -0500

    adds functional rna-transcription

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,6 +1,17 @@
 
 module.exports = toRna;
 
-function toRna(){
-	
+var DnaToRna = {
+	C: "G",
+	G: "C",
+	A: "U",
+	T: "A"
+};
+
+function toRna(Dna){
+	Rna = "";
+  for (var i = 0, x = Dna.length; i < x; i++) {
+    Rna += DnaToRna[Dna[i]];
+  }
+  return Rna;
 }
